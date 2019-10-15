888c65a5b06132d7d20f98761027c5ed5bad10df
JHKim <didwjd169@gmail.com>
2019-10-15 17:50:50 +0900

    1015_update

--- sw_2105.py
+++ sw_2105.py
@@ -2,27 +2,43 @@ import sys
 sys.stdin = open('sw_2105.txt', 'r')
 
 
-def is_inbox(y, x):
-    if 0 <= y < N and 0 <= x < N:
+def is_good(x):
+    if 1 <= x < N-1:
         return True
     return False
 
 
-def solve(p, a, b, k, di_lst):
-    global starting_point
-    global i
-    y = p[0]
-    x = p[1]
-    tg = total_map[y][x]
-    if tg in di_lst:
-        return
+def get_end(x, L, x_lst):
+    if L%2 == 1:
+        x_lst.append(x)
+        L -= 1
+        for i in range(1, L//2+1):
+            ax = x - (2*i)
+            bx = x + (2*i)
+            if is_good(ax):
+                x_lst.append(ax)
+            if is_good(bx):
+                x_lst.append(bx)
     else:
-        di_lst.append(tg)
-        yy = y + dif[k][0]
-        xx = x + dif[k][1]
-        new_p = (yy, xx)
-        if is_inbox(yy, xx):
-            solve(new_p, a+1, b, di_lst)
+        for ax in range(x-L+1, x+L, 2):
+            if is_good(ax):
+                x_lst.append(ax)
+    return x_lst
+
+
+def solve(ay, ax, by):
+    L = by - ay - 1
+    x_lst = get_end(ax, L, list())
+    for tx in x_lst:
+        d_lst = [0, 0]
+        if ax == tx:
+            aa = (L+1)//2
+            bb = aa
+        else:
+
+        d_lst[0] = aa
+        d_lst[1] = bb
+        print(ay, ax, by, tx, d_lst)
 
 
 dif = [(1, 1), (1, -1), (-1, -1), (-1, 1)]
@@ -30,9 +46,9 @@ TC = int(input())
 for testcase in range(1, TC+1):
     N = int(input())
     total_map = [list(map(int, input().split())) for _ in range(N)]
-    for y in range(N-2):
-        for x in range(1, N-1):
-            di_lst = []
-            starting_point = (y, x)
-            solve(starting_point, 0, 0, 0, di_lst)
-
+    result = -1
+    for ay in range(0, N-2):
+        for by in range(ay+2, N):
+            for ax in range(1, N-1):
+                solve(ay, ax, by)
+    print('#{} {}'.format(testcase, result))
