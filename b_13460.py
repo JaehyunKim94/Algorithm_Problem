27fb0f048aad4f4f7b49ef097e25de1157f2caea
JHKim <didwjd169@gmail.com>
2019-10-30 18:00:14 +0900

    1030_update

--- b_13460.py
+++ b_13460.py
@@ -8,6 +8,7 @@ def move(q, i):
     dif = [(0, -1), (0, 1), (-1, 0), (1, 0)]
     d = dif[i]
     r_y, r_x, b_y, b_x = q[0], q[1], q[2], q[3]
+    ck = 0
     while total_map[r_y][r_x] != '#':
         r_y += d[0]
         r_x += d[1]
@@ -23,23 +24,34 @@ def move(q, i):
             break
 
     if ck != 0:
-        return '1'
+        return [ck]
 
     r_y -= d[0]
     r_x -= d[1]
     b_y -= d[0]
     b_x -= d[1]
     if r_y == b_y and r_x == b_x:
-        if i in (0, 1):
+        if i == 0:
+            if q[1] < q[3]:
+                b_x += 1
+            else:
+                r_x += 1
+        elif i == 1:
             if q[1] > q[3]:
                 b_x -= 1
             else:
                 r_x -= 1
-        elif i in (2, 3):
+        elif i == 2:
+            if q[0] > q[2]:
+                r_y += 1
+            else:
+                b_y += 1
+        elif i == 3:
             if q[0] > q[2]:
                 b_y -= 1
             else:
                 r_y -= 1
+
     res = (r_y, r_x, b_y, b_x, i)
     return res
 
@@ -50,6 +62,7 @@ def solve(red, blue):
     r_y, r_x = red[0], red[1]
     b_y, b_x = blue[0], blue[1]
     que = [(r_y, r_x, b_y, b_x, -1)]
+    ck_lst = []
     for t in range(1, 11):
         r = len(que)
         for _ in range(r):
@@ -59,13 +72,14 @@ def solve(red, blue):
                     new = move(q, i)
                     if len(new) > 1:
                         que.append(new)
-        if ck < 0:
-            result = -1
-            return
-        elif ck > 0:
-            result = t
-            return
-    result = -1
+                    else:
+                        new = new[0]
+                        ck_lst.append((new, t))
+
+    for c in ck_lst:
+        if c[0] > 0:
+            result = min(result, c[1])
+    return
 
 
 N, M = map(int, input().split())
@@ -81,7 +95,9 @@ for y in range(N):
         elif new_lst[x] == 'O':
             hole = (y, x)
         total_map[y][x] = new_lst[x]
-result = 0
+result = 99999
 ck = 0
 solve(red, blue)
+if result == 99999:
+    result = -1
 print(result)
