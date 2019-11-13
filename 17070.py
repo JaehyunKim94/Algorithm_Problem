77d118cf2e90960e75136b512a35baec92a1904b
Jaehyun Kim <didwjd169@gmail.com>
2019-11-13 19:58:30 +0900

    last commit on 11_13

--- 17070.py
+++ 17070.py
@@ -1,15 +1,30 @@
-import sys
-sys.stdin = open('17070.txt', 'r')
-import time
-
 def solve():
-    pass
+    dif = [(0, 1), (1, 1), (1, 0)]
+    que = [(0, 1, 0)]
+    cnt = 0
+    while que:
+        y, x, d = que.pop()
+        if y == N-1 and x == N-1:
+            cnt += 1
+            continue
+        ac, bc = False, False
+        for i in (0, 2, 1):
+            dy, dx = dif[i]
+            yy = y + dy
+            xx = x + dx
+            if yy < N and xx < N and total_map[yy][xx] == 0:
+                if i == 0:
+                    ac = True
+                    if d in (0, 1):
+                        que.append((yy, xx, i))
+                elif i == 2:
+                    bc = True
+                    if d in (1, 2):
+                        que.append((yy, xx, i))
+                elif i == 1 and ac and bc:
+                    que.append((yy, xx, 1))
+    return cnt
 
-
-st = time.time()
 N = int(input())
 total_map = [list(map(int, input().split())) for _ in range(N)]
-cnt_map = [[0] * M for _ in range(N)]
 print(solve())
-
-print(time.time() - st)
